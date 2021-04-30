0a7d45a32a95c6078667f7bf1c0a6f1bf05ea9b0
Sriram Patibanda <patibandasriram2k02@gmail.com>
2021-05-01 00:27:05 +0530

    generates gitignore file using the needed techologies

--- gitignore-generator-/src/extension.ts
+++ gitignore-generator-/src/extension.ts
@@ -1,27 +1,32 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import axios from 'axios';
+import {writeFile}  from 'fs';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "gitignore-generator-" is now active!');
+	console.log('Congratulations, your extension "gitignore-generator" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('gitignore-generator-.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('gitignore-generator', async () => {
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from gitignore generator !');
+		try {
+			vscode.window.showInformationMessage('Enter framework, languages, code editors, etc.');
+	
+			const keywords = await vscode.window.showInputBox();
+			const response = await axios.get(`https://www.toptal.com/developers/gitignore/api/${keywords}`);
+
+			if (response) {
+				writeFile(`${__dirname}/.gitignore`, response.data, {encoding: 'utf8'}, (error) => {
+					console.log('write file sync', error);
+					vscode.window.showInformationMessage('.gitignore file created');
+				});
+			}
+			
+		} catch (error) {
+			console.log ('Try Catch Error: ', error);
+		}		
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
